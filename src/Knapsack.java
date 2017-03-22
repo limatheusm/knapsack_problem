c72668845da1a162118933075db01e6cd359e6f9
Matheus Lima Moura de Araujo <matheus.lima@cc.ci.ufpb.br>
2017-03-21 22:43:23 -0300

    uP

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -5,13 +5,10 @@ class Knapsack {
 	
 	public static void main(String[] args) {
 
-        BufferedReader inReader = 
-        				new BufferedReader(new InputStreamReader(System.in));
+        	BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+		int w = 0, n = 0;
 
-        int w = 0, 
-        	n = 0;
-
-        try {
+        	try {
 
         	//primeira linha do arquivo
         	String arr [] = new String [2];
@@ -22,26 +19,25 @@ class Knapsack {
         	int val[] = new int[n], 
         		wt[] = new int[n];
 
-            //segunda linha do arquivo (Pesos dos itens)
+            	//segunda linha do arquivo (Pesos dos itens)
         	arr = new String[n];
-            arr = inReader.readLine().split(" ");
-            for (int i = 0; i < arr.length; i++) 
-            	wt[i] = Integer.parseInt(arr[i]);
+           	 arr = inReader.readLine().split(" ");
+            	for (int i = 0; i < arr.length; i++) 
+            		wt[i] = Integer.parseInt(arr[i]);
 
-            //terceira linha do arquivo (Valores dos itens)
-            arr = new String[w];
-            arr = inReader.readLine().split(" ");
-            for (int i = 0; i < arr.length; i++) 
-            	val[i] = Integer.parseInt(arr[i]);
+            	//terceira linha do arquivo (Valores dos itens)
+            	arr = new String[w];
+            	arr = inReader.readLine().split(" ");
+            	for (int i = 0; i < arr.length; i++) 
+            		val[i] = Integer.parseInt(arr[i]);
 
-            inReader.close();
+            	inReader.close();
 
-            System.out.println("Valor: "+knapsack(val, wt, w));
+            	System.out.println("Valor: "+knapsack(val, wt, w));
 
-
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
+        	} catch (IOException e) {
+            		System.err.println(e.getMessage());
+        	}
 	}
 
 
@@ -74,9 +70,8 @@ class Knapsack {
 				if (wItem[item - 1] <= knapsackWeight)
 					// retorna o max entre a soma do valor do item atual e o do item (pesoMochila - pesoItem) 
 							// e o valor do item da linha anterior e da mesma coluna
-					m [item][knapsackWeight] = Math.max 
-										(val[item - 1] + m[item - 1][ knapsackWeight-wItem[item - 1] ], 
-											m[item - 1][knapsackWeight]);
+					m [item][knapsackWeight] = Math.max(val[item - 1] + m[item - 1][ knapsackWeight-wItem[item - 1] ], 
+										m[item - 1][knapsackWeight]);
 				else
 					//Caso nao caiba na mochila, repete o valor do item da linha anterior de mesma coluna
 					m [item][knapsackWeight] = m[item - 1][knapsackWeight];
@@ -84,14 +79,14 @@ class Knapsack {
 		}
 
 		//exibindo a tabela
-        /*for (int[] rows : m) {
-            for (int col : rows) {
-                System.out.format("%5d", col);
-            }
-            System.out.println();
-        }*/
+        	/*for (int[] rows : m) {
+            		for (int col : rows) {
+                	System.out.format("%5d", col);
+            		}
+            	System.out.println();
+        	}*/
 
-        //Processo de decisao dos produtos
+        	//Processo de decisao dos produtos
 		ArrayList<Integer> selectedItems = new ArrayList<Integer>();
 		int colWeightKnapsack = wKnapsack;
 		for (int item = n; item >= 1; item--) {
@@ -106,5 +101,4 @@ class Knapsack {
 		//ultimo elemento da tabela sera o valor maximo possivel
 		return m[n][wKnapsack];
 	}
-
 }
